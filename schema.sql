0bf1213fc5a9b190d85bfbf7e9d6485bd91781e3
Sean Flinn <Sean.Flinn1@gmail.com>
2022-04-25 15:49:35 -0400

    push new schema built

---
+++ schema.sql
@@ -0,0 +1,62 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+	dept_no varchar(4) NOT NULL,
+	dept_name varchar(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+
+-- Create employees table for PH-EmployeeDB
+CREATE TABLE employees (
+	emp_no int NOT NULL,
+	birth_date date NOT NULL,
+	first_name varchar NOT NULL,
+	last_name varchar NOT NULL,
+	gender varchar NOT NULL,
+	hire_date date NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+-- Create dept_manager table for PH-EmployeeDB
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no int NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create salaries table
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create Titles table
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM departments;
