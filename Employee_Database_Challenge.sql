e648a7757c4736a6ebe17d6048f405d9b9eed766
Sean Flinn <Sean.Flinn1@gmail.com>
2022-04-26 10:22:21 -0400

    Final submission

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -9,9 +9,10 @@ INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
 	ON(e.emp_no = ti.emp_no)
+INNER JOIN dept_emp as de
+	on (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
+AND (de.to_date = '9999-01-01');
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -31,15 +32,20 @@ FROM unique_titles
 GROUP BY title
 ORDER BY count(emp_no) DESC;
 
--- Create a table of employees Retirement Titles
-SELECT e.emp_no,
+-- Create a table of Mentorship eligibility
+SELECT DISTINCT ON (emp_no) e.emp_no,
 	e.first_name,
 	e.last_name,
-	ti.title,
-	ti.from_date,
-	ti.to_date
-INTO retirement_titles
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
 FROM employees as e
+INNER JOIN dept_emp as de
+	ON(e.emp_no = de.emp_no)
 INNER JOIN titles as ti
 	ON(e.emp_no = ti.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
